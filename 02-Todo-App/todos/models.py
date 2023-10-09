c7f61cbae7de4a38b68b03e03dee15775d051963
Kumail Ali Shaikh <kumailalidev@gmail.com>
2023-10-10 03:04:14 +0500

    P02-Removed description field from Todo model

--- 02-Todo-App/todos/models.py
+++ 02-Todo-App/todos/models.py
@@ -7,7 +7,6 @@ class Todo(models.Model):
     """
 
     title = models.CharField(max_length=250)
-    description = models.TextField(blank=True, null=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
