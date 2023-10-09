5d88147551c74c621b3ce33cc1284d80912550ca
Kumail Ali Shaikh <kumailalidev@gmail.com>
2023-10-10 00:10:55 +0500

    P02-Created home view function

--- 02-Todo-App/todos/views.py
+++ 02-Todo-App/todos/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
 
-# Create your views here.
+from .models import Todo
+
+
+def home(request):
+    """
+    home view function to display list of todo objects.
+    """
+    todos = Todo.objects.all()
+    context = {
+        "todos": todos,
+    }
+
+    return render(request, "todos/index.html", context)
