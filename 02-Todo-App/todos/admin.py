4623bf3730ee4f82ba6010d1c984d906515fba91
Kumail Ali Shaikh <kumailalidev@gmail.com>
2023-10-10 00:05:55 +0500

    P02-Registered Todo model to administration page

--- 02-Todo-App/todos/admin.py
+++ 02-Todo-App/todos/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Todo
+
+
+class TodoAdmin(admin.ModelAdmin):
+    list_display = ["title", "created_at", "updated_at"]
+
+
+admin.site.register(Todo, TodoAdmin)
