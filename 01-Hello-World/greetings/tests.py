6aa3b268c31fcdc1c3fe6ce4b096384bd5be0e02
Kumail Ali Shaikh <kumailalidev@gmail.com>
2023-10-09 17:30:09 +0500

    P01-Write test for home view

--- 01-Hello-World/greetings/tests.py
+++ 01-Hello-World/greetings/tests.py
@@ -1,3 +1,11 @@
 from django.test import TestCase
+from django.urls import reverse
 
-# Create your tests here.
+
+class GreetingsTests(TestCase):
+    def test_home_view(self):
+        """
+        Function to test home view.
+        """
+        response = self.client.get(reverse("home"))
+        self.assertEqual(response.status_code, 200)
