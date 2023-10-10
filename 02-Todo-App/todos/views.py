1002f8456a560320889f409a824f2108f8a6339a
Kumail Ali Shaikh <kumailalidev@gmail.com>
2023-10-10 18:04:09 +0500

    P02-Added number of incomplete todos to home view

--- 02-Todo-App/todos/views.py
+++ 02-Todo-App/todos/views.py
@@ -8,8 +8,11 @@ def home(request):
     home view function to display list of todo objects.
     """
     todos = Todo.objects.all()
+    incomplete_todos = todos.filter(completed=False).count()
+
     context = {
         "todos": todos,
+        "incomplete_todos": incomplete_todos,
     }
 
     return render(request, "todos/index.html", context)
