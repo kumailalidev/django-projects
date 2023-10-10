bfe670a7a4d5c20e95396b5d748d4eed3f46a819
Kumail Ali Shaikh <kumailalidev@gmail.com>
2023-10-10 17:57:44 +0500

    P02-Added completed boolean field to Todo model

--- 02-Todo-App/todos/models.py
+++ 02-Todo-App/todos/models.py
@@ -7,6 +7,7 @@ class Todo(models.Model):
     """
 
     title = models.CharField(max_length=250)
+    completed = models.BooleanField(default=False)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
