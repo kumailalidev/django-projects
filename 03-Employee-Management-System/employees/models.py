4f427d1b0f9eb1cb7aa005ebdf7085b1c7492d29
Kumail Ali Shaikh <kumailalidev@gmail.com>
2023-10-11 23:28:05 +0500

    P03-Created basic database model of Employee

--- 03-Employee-Management-System/employees/models.py
+++ 03-Employee-Management-System/employees/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+
+class Employee(models.Model):
+    first_name = models.CharField(max_length=50)
+    middle_name = models.CharField(blank=True, null=True, max_length=50)
+    last_name = models.CharField(max_length=50)
+    age = models.IntegerField()
+    date_of_birth = models.DateField()
+    email = models.EmailField()
+    phone = models.CharField(max_length=15)
+    designation = models.CharField(max_length=250)
