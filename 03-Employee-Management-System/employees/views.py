78d792f8ae0bcad2c837925f2cb099263f6d3cd4
Kumail Ali Shaikh <kumailalidev@gmail.com>
2023-10-12 04:21:36 +0500

    P03-Created template add_employees.html for adding employees

--- 03-Employee-Management-System/employees/views.py
+++ 03-Employee-Management-System/employees/views.py
@@ -3,3 +3,7 @@ from django.shortcuts import render
 
 def home(request):
     return render(request, "employees/index.html", {})
+
+
+def add(request):
+    return render(request, "employees/add_employee.html", {})
