be5d119b223f1816cef1f143a3e0b87249f77c55
Kumail Ali Shaikh <kumailalidev@gmail.com>
2023-10-11 23:58:16 +0500

    P03-Created basic database model Education for storing employee education

--- 03-Employee-Management-System/employees/models.py
+++ 03-Employee-Management-System/employees/models.py
@@ -10,3 +10,14 @@ class Employee(models.Model):
     email = models.EmailField()
     phone = models.CharField(max_length=15)
     designation = models.CharField(max_length=250)
+
+
+class Education(models.Model):
+    employee = models.ForeignKey(Employee, on_delete=models.CASCADE)
+    institute_name = models.CharField(max_length=250)
+    degree_title = models.CharField(max_length=250)
+    field_of_study = models.CharField(max_length=50)
+    start_date = models.DateField()
+    end_date = models.DateField(blank=True, null=True)
+    grade = models.CharField(max_length=10)
+    description = models.TextField(blank=True, null=True)
