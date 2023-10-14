cff467a8122fdb3dc6f4a96eac4ba19b76dd3b5f
Kumail Ali Shaikh <kumailalidev@gmail.com>
2023-10-14 19:17:19 +0500

    P03-Updated Employee model, added gender field and education one-to-one field

--- 03-Employee-Management-System/employees/models.py
+++ 03-Employee-Management-System/employees/models.py
@@ -1,24 +1,7 @@
 from django.db import models
 
 
-class Employee(models.Model):
-    first_name = models.CharField(max_length=50)
-    middle_name = models.CharField(blank=True, null=True, max_length=50)
-    last_name = models.CharField(max_length=50)
-    age = models.IntegerField()
-    date_of_birth = models.DateField()
-    email = models.EmailField()
-    phone = models.CharField(max_length=15)
-    designation = models.CharField(max_length=250)
-
-    def __str__(self):
-        return f"{self.first_name} {self.middle_name if self.middle_name else ''} {self.last_name}"
-
-
 class Education(models.Model):
-    employee = models.ForeignKey(
-        Employee, on_delete=models.CASCADE, related_name="educations"
-    )
     institute_name = models.CharField(max_length=250)
     degree_title = models.CharField(max_length=250)
     field_of_study = models.CharField(max_length=50)
@@ -27,5 +10,32 @@ class Education(models.Model):
     grade = models.CharField(max_length=10)
     description = models.TextField(blank=True, null=True)
 
+
+class Employee(models.Model):
+    # gender choices as a list of tuples
+    GENDER_CHOICES = [
+        ("M", "Male"),
+        ("F", "Female"),
+        ("O", "Other"),
+    ]
+
+    # basic information
+    first_name = models.CharField(max_length=50)
+    middle_name = models.CharField(blank=True, null=True, max_length=50)
+    last_name = models.CharField(max_length=50)
+    age = models.IntegerField()
+    gender = models.CharField(max_length=1, choices=GENDER_CHOICES, default="O")
+    email = models.EmailField()
+    phone = models.CharField(max_length=15)
+    designation = models.CharField(max_length=250)
+
+    # education
+    education = models.OneToOneField(
+        Education,
+        on_delete=models.SET_NULL,
+        related_name="education_details",
+        null=True,
+    )
+
     def __str__(self):
-        return self.employee.__str__()
+        return f"{self.first_name} {self.middle_name if self.middle_name else ''} {self.last_name}"
