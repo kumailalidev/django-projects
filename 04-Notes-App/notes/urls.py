59c7fc343dedab6eb670220fb38ae97f06ae4249
Kumail Ali Shaikh <kumailalidev@gmail.com>
2023-10-19 23:20:17 +0500

    p04-Created class based home view and url.

--- 04-Notes-App/notes/urls.py
+++ 04-Notes-App/notes/urls.py
@@ -1,3 +1,7 @@
 from django.urls import path
+from . import views
 
-urlpatterns = []
+
+urlpatterns = [
+    path("", views.HomeView.as_view(), name="home"),
+]
--- 04-Notes-App/notes/views.py
+++ 04-Notes-App/notes/views.py
@@ -1,3 +1,11 @@
+from django.http import HttpResponse
 from django.shortcuts import render
+from django.views import View
 
-# Create your views here.
+
+class HomeView(View):
+    template_name = "index.html"
+
+    # handle GET request
+    def get(self, request, *args, **kwargs):
+        return HttpResponse("Django Notes App")
