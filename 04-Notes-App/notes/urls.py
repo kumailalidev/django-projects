e4e8af8e8d2195d6d273ea2a7e5239f10d863675
Kumail Ali Shaikh <kumailalidev@gmail.com>
2023-10-20 04:27:51 +0500

    p04-Created login view and template using Django default authentication

--- 04-Notes-App/notes/urls.py
+++ 04-Notes-App/notes/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
+from django.contrib.auth import views as auth_views
+
 from . import views
 
 
 urlpatterns = [
     path("", views.HomeView.as_view(), name="home"),
+    path("accounts/login/", auth_views.LoginView.as_view(), name="login"),
 ]
