1c30452bdb4b4d9f27ef5477625cd4ef6278aad9
Kumail Ali Shaikh <kumailalidev@gmail.com>
2023-10-20 04:46:18 +0500

    p04-Created logout view and template for using Django default authentication

--- 04-Notes-App/notes/urls.py
+++ 04-Notes-App/notes/urls.py
@@ -7,4 +7,5 @@ from . import views
 urlpatterns = [
     path("", views.HomeView.as_view(), name="home"),
     path("accounts/login/", auth_views.LoginView.as_view(), name="login"),
+    path("accounts/logout/", auth_views.LogoutView.as_view(), name="logout"),
 ]
