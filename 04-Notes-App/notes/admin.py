7c63126b25e9ab450bac4ceab63e5e5321c28664
Kumail Ali Shaikh <kumailalidev@gmail.com>
2023-10-23 00:40:01 +0500

    p04-Created a Note model and registered to admin site

--- 04-Notes-App/notes/admin.py
+++ 04-Notes-App/notes/admin.py
@@ -1,6 +1,7 @@
+from typing import Any
 from django.contrib import admin
 
-from .models import Tag
+from .models import Note, Tag
 
 
 class TagAdmin(admin.ModelAdmin):
@@ -14,3 +15,30 @@ class TagAdmin(admin.ModelAdmin):
 
 
 admin.site.register(Tag, TagAdmin)
+
+
+class NoteAdmin(admin.ModelAdmin):
+    # display tags related to note
+    def tags_list(self, obj):
+        tags = ", ".join([str(tag) for tag in obj.tags.all()])
+        return tags if tags else "None"
+
+    tags_list.short_description = "Related tags"
+
+    list_display = (
+        "title",
+        "user",
+        "status",
+        "tags_list",
+        "updated_at",
+    )
+
+    # override save_model method to automatically assign
+    # currently logged-in admin as user
+    def save_model(self, request, obj, form, change):
+        obj.user = request.user
+        obj.save()
+        return super().save_model(request, obj, form, change)
+
+
+admin.site.register(Note, NoteAdmin)
--- 04-Notes-App/notes/models.py
+++ 04-Notes-App/notes/models.py
@@ -1,8 +1,14 @@
 from django.db import models
 from django.utils.text import slugify
+from django.contrib.auth.models import User
+from django.contrib.auth import get_user
 
 
 class Tag(models.Model):
+    """
+    Tag model
+    """
+
     name = models.CharField(unique=True, max_length=50)
     slug = models.SlugField(unique=True, blank=True)
 
@@ -18,3 +24,35 @@ class Tag(models.Model):
 
     def __str__(self):
         return self.name
+
+
+class Note(models.Model):
+    """
+    Note model
+    """
+
+    # choices for status field
+    class Status(models.TextChoices):
+        DRAFT = ("DF", "Draft")
+        PUBLISHED = ("PB", "Published")
+
+    user = models.ForeignKey(User, on_delete=models.CASCADE, blank=True)
+    title = models.CharField(max_length=150)
+    body = models.TextField()
+    tags = models.ManyToManyField(Tag, related_name="notes", blank=True)
+    pinned = models.BooleanField(default=False)
+    archived = models.BooleanField(default=False)
+    status = models.CharField(
+        max_length=2, choices=Status.choices, default=Status.DRAFT
+    )
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    # overriding save method to assign current logged in user automatically
+    def save(self, *args, **kwargs):
+        if self.user_id == None:
+            self.user = get_user(self)  # assign the current logged-in user
+        super(Note, self).save(*args, **kwargs)
+
+    def __str__(self):
+        return self.title
