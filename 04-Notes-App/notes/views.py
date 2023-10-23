94ff7d46db9ade3cd4303cd14788e220a3025c19
Kumail Ali Shaikh <kumailalidev@gmail.com>
2023-10-23 05:52:45 +0500

    p04-Created HomeView inherited from TemplateView and updated index.html template to render notes created by user in database

--- 04-Notes-App/notes/views.py
+++ 04-Notes-App/notes/views.py
@@ -1,18 +1,26 @@
-from django.shortcuts import redirect, render
+from django.shortcuts import redirect
 from django.urls import reverse_lazy
-from django.views import View
-from django.views import generic
+from django.views.generic import TemplateView, CreateView
 from django.contrib.auth.forms import UserCreationForm
 
+from .models import Note
 
-class HomeView(View):
+
+class HomeView(TemplateView):
     template_name = "index.html"
 
-    # handle GET request
-    def get(self, request, *args, **kwargs):
-        return render(request, "index.html", {})
+    # set context data
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+
+        # published notes
+        notes = Note.published.filter(user=self.request.user)
+        context["notes"] = notes
+
+        return context
 
     # override dispatch method to check whether user is logged in or not
+    # NOTE: This can be achieved using LoginRequiredMix class as base class.
     def dispatch(self, request, *args, **kwargs):
         if not request.user.is_authenticated:
             return redirect("login")
@@ -20,7 +28,7 @@ class HomeView(View):
         return super().dispatch(request, *args, **kwargs)
 
 
-class UserRegistrationView(generic.CreateView):
+class UserRegistrationView(CreateView):
     template_name = "registration/user_registration_form.html"
     form_class = UserCreationForm
     success_url = reverse_lazy("login")
